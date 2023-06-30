dd356605c9e9d5a7a81fddf9da947a2a1a5f3110
Vergazov_A <vergazka@gmail.com>
2023-06-30 16:06:30 +0300

    рабочий вариант с композером

---
+++ application/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2a8f1e21dc390926dadd2970fe5d1ceadcfa2ea8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2a8f1e21dc390926dadd2970fe5d1ceadcfa2ea8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

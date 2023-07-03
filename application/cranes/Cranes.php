e7014bedcd2104bcd8d098a14401c71808a92ab6
Vergazov_A <vergazka@gmail.com>
2023-07-03 16:30:25 +0300

    правлю диспетчер

--- application/cranes/Cranes.php
+++ application/cranes/Cranes.php
@@ -31,12 +31,3 @@ class Cranes
     }
 }
 
-$Cranes = [
-    new Cranes("КП-1550",50,12),
-    new Cranes("КП-0042",42,12),
-    new Cranes("КП-0070",70,8),
-    new Cranes("КПМ-0215",15,25),
-    new Cranes("КПМ-0315",15,26),
-    new Cranes("КПМ-0220",20,20),
-];
-
--- application/cranes/Dispatcher.php
+++ application/cranes/Dispatcher.php
@@ -2,12 +2,26 @@
 
 namespace App;
 
+$cranesList = [
+    new Cranes("КП-1550",50,12),
+    new Cranes("КП-0042",42,12),
+    new Cranes("КП-0070",70,8),
+    new Cranes("КПМ-0215",15,25),
+    new Cranes("КПМ-0315",15,26),
+    new Cranes("КПМ-0220",20,20),
+];
 class Dispatcher
 {
-    public function getRightCranes($weight,$distance): string
+
+    public function getRightCranes($weight,$distance,$cranesList): string
     {
-       $rightCranes = (new CranesSearcher())->searchCranes($cargo, (new Cranes())->getCranesList());
+       $rightCranes = (new CranesSearcher())->search($cargo, (new Cranes())->getCranesList());
         return (new MessageCreator())->create($rightCranes, $cargo);
-        $rightCranes = (new CranesSearcher())->search($weight,$distance, (new Cranes())->getCranesList());
+
+        foreach ($cranesList as $crane){
+            $rightCranes = (new CranesSearcher())->search($cargoWeight,$cargoDistance,$craneWeight,$craneDistance);
+        }
+
     }
+
 }
